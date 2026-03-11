a1bf86d51d4fcb5fa0aa0c7c70d385e21d35e002
ashiii1 <aashishaik526@gmail.com>
2026-03-11 15:49:35 +0530

    hospital

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -30,7 +30,7 @@ function Hospital() {
       </button>
 
       <iframe
-        src="https://hos-rouge.vercel.app/"
+        src="https://hos-cgfqrrlyn-ashis-projects-e33ce122.vercel.app/"
         style={{ width: "100%", height: "100vh", border: "none" }}
         title="Hospital"
       ></iframe>
